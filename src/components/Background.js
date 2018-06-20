a8cde2a16f6ea2cec46dfe4b0b25e94bae681364
nickbreaton <nick@breaton.com>
2018-06-19 22:06:21 -0400

    Clean up

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,6 +3,30 @@ import { getStatus, formatDuration } from '../lib/core'
 import moment from 'moment'
 import React from 'react'
 
+const desktopBackground = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  minHeight: '100vh',
+  width: '100vw',
+  background: '#36454f',
+  position: 'relative',
+  ['&::before']: {
+    content: '""',
+    display: 'inline-block',
+    position: 'absolute',
+    background: 'url(/background.jpg)',
+    backgroundSize: 'cover',
+    filter: 'blur(1rem)',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1,
+    opacity: 0.25
+  }
+})
+
 const containerStyle = css({
   minHeight: '100vh',
   backgroundImage: `
@@ -11,7 +35,15 @@ const containerStyle = css({
   `,
   backgroundSize: 'auto 125%',
   backgroundPosition: '25%',
-  padding: '3rem 1rem'
+  padding: '3rem 1rem',
+  width: '100%',
+  zIndex: 2,
+  ['@media screen and (min-width: 769px)']: {
+    width: '375px',
+    minHeight: '667px',
+    borderRadius: '0.5rem',
+    boxShadow: '0 0 1rem 0 rgba(0,0,0,0.333)'
+  }
 })
 
 const headingStyle = css({
@@ -43,11 +75,13 @@ export const timeStyle = css(titleStyle, {
 })
 
 export const Background = ({ children }) => (
-  <div className={containerStyle}>
-    <div className={headingsStyle}>
-      <h1 className={headingStyle}>Iowa City, IA</h1>
-      <h2 className={subheadingStyle}>Where can I park?</h2>
+  <div className={desktopBackground}>
+    <div className={containerStyle}>
+      <div className={headingsStyle}>
+        <h1 className={headingStyle}>Iowa City, IA</h1>
+        <h2 className={subheadingStyle}>Where can I park?</h2>
+      </div>
+      {children}
     </div>
-    {children}
   </div>
 )
